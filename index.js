aa7213263c7cfcffda4dbdd734a5c419fb8948c7
Trevor Karuma <trevormukundi@gmail.com>
2025-03-12 01:15:41 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sean Njeru",
+    streetAddress: "128 Main St"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue() {
+    employee.name = "Sam"
+    employee.streetAddress = "12 Broadway"
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey() {
+    const newEmployee = { ...employee };
+    delete employee.name;
+    delete employee.streetAddress;
+    return employee;
+}
